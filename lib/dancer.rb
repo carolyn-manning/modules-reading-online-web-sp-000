3f05077c417ea5c024d283fee98496568bf873b0
Carolyn Manning <carolyn.manning3@gmail.com>
2020-10-30 02:28:30 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,15 +3,16 @@ require_relative './dance_module.rb'
 
 class Dancer
 
-  include Dance
-
   attr_accessor :name
 
   def initialize(name)
     @name = name
   end
 
-  extend MetaDancing
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+
 
 
 end
